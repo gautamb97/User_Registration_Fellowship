c571f1f2620d350e0e5b3247b768279edc037935
gautamb97 <www.gautam971997@gmail.com>
2020-12-20 18:33:34 +0530

    Refactor:naming_convention

--- UserRegistration.java
+++ UserRegistration.java
@@ -1,72 +1,68 @@
 import java.util.Scanner;
 import java.util.regex.*;
 public class UserRegistration {
-    public void firstName(){
-        Scanner firstName = new Scanner(System.in);
+    static Scanner userInput = new Scanner(System.in);
+    public void firstNameVerification(){
         System.out.println("Enter your first name: ");
-        String first_name = firstName.next();
+        String first_name = userInput.next();
         if(Pattern.matches("^[A-Z]{1}[A-Za-z]{2,}$",first_name)==true)
             System.out.println(first_name);
         else {
             System.out.println("invalid first name re-enter your name");
-            firstName();
+            firstNameVerification();
         }
     }
     //last name validation
-    public void lastName() {
-        Scanner lastName = new Scanner(System.in);
+    public void lastNameVerification() {
         System.out.println("Enter your last name: ");
-        String last_name = lastName.next();
+        String last_name = userInput.next();
         if (Pattern.matches("^[A-Z]{1}[A-Za-z]{2,}$", last_name) == true)
             System.out.println(last_name);
         else {
             System.out.println("invalid last name re-enter your name");
-            lastName();
+            lastNameVerification();
         }
     }
     //email verification
-    public void email() {
-        Scanner email = new Scanner(System.in);
+    public void emailVerification() {
         System.out.println("Enter your email id: ");
-        String email_verification = email.next();
+        String email_verification = userInput.next();
         if (Pattern.matches("^[a-z0-9\\.\\_\\-\\+a-z0-9]*\\@[a-z0-9]*\\.(com|org|in|co|net|com\\.au|com\\.com)", email_verification) == true)
             System.out.println(email_verification);
         else {
             System.out.println("invalid email id re-enter your email id");
-            email();
+            emailVerification();
         }
     }
     //mobile number verification
-    public void mobileNumber(){
-        Scanner mobileNumber = new Scanner(System.in);
+    public void mobileNumberVerification(){
         System.out.println("Enter your mobile number: ");
-        String mobile_number_verification = mobileNumber.next();
+        String mobile_number_verification = userInput.next();
         if (Pattern.matches("^(\\+91)?[6-9]{1}[0-9]{9}$", mobile_number_verification) == true)
             System.out.println(mobile_number_verification);
         else {
             System.out.println("invalid mobile number re-enter your mobile number");
-            mobileNumber();
+            mobileNumberVerification();
         }
     }
     //Password rules passing
-    public void password(){
-        Scanner password = new Scanner(System.in);
+    public void passwordVerification(){
         System.out.println("Enter your password: ");
-        String password_verification = password.next();
+        String password_verification = userInput.next();
         if (Pattern.matches("^(?=.*[A-Z])(?=.*[0-9])(?=.*\\W)[a-zA-Z0-9\\#]{8,}$", password_verification) == true)
-            System.out.println(password_verification);
+            System.out.println("*******************");
         else {
             System.out.println("invalid password re-enter your password");
-            password();
+            passwordVerification();
         }
     }
     public static void main(String[] args){
         System.out.println("Welcome for Registration");
         UserRegistration validation = new UserRegistration();
-        validation.firstName();
-        validation.lastName();
-        validation.email();
-        validation.mobileNumber();
-        validation.password();
+        validation.firstNameVerification();
+        validation.lastNameVerification();
+        validation.emailVerification();
+        validation.mobileNumberVerification();
+        validation.passwordVerification();
     }
 }
